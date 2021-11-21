5ed9bb6b0f33a1bee68adc60208ff04791448a13
kmamonnassi <ka82573@jk2.so-net.ne.jp>
2021-11-21 17:04:39 +0900

    KSG制作所についてなど

--- index.js
+++ index.js
@@ -1,3 +1,14 @@
+const swipe_wrapper = document.getElementsByClassName("swiper-wrapper")[0];
+
+function Add(contentsName) {
+	swipe_wrapper.insertAdjacentHTML("beforeend", '<div class="swiper-slide"><a href="#"><img src="game_scrolls/' + contentsName + '/screenshot_1.png"></a></div>');
+}
+
+Add("KARAGE");
+Add("Disjointed");
+Add("獄落道中");
+
+//スワイプでスクロールできるように
 var mySwiper = new Swiper('.swiper-container', {
 	loop: true,
 	navigation: {
@@ -9,4 +20,11 @@ var mySwiper = new Swiper('.swiper-container', {
 		type: 'bullets',
 		clickable: true,
 	},
+	autoplay: {
+		delay: 3000,
+		stopOnLastSlide: false,
+		disableOnInteraction: false,
+		reverseDirection: false
+	},
+	effect: 'coverflow',
 });
